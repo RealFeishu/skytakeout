f7a76cdcbd8290da34f19764e3ff9ae6f3e73db0
FEISHU <961560324@qq.com>
2023-09-15 21:46:38 +0800

    编辑员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -38,4 +38,11 @@ public interface EmployeeMapper {
      * 根据主键动态修改属性
      */
     void update(Employee employee);
+
+    /**
+     * 根据id查询用户
+     * @param id
+     */
+    @Select("select * from employee where id=#{id}")
+    Employee getById(Long id);
 }
